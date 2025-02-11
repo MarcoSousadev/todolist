c255cfc1df5d8138fede41fbf6aadaf1e8756033
Marco Sousa <marcotgousa@gmail.com>
2025-02-10 21:30:15 -0300

    Feat: Implemented a task counter

--- src/components/TaskArea.jsx
+++ src/components/TaskArea.jsx
@@ -16,77 +16,82 @@ export function TaskArea (){
   function handleCreateNewTask() {
     event.preventDefault()
 
-    
-    
+    setTask([...task, newTaskInputText])
 
-    setTask([...task, newTaskInputText]);
-
-    setNewTaskInputText('');
+    setNewTaskInputText('')
   }
 
-  function handleNewTextChange(){
+  function handleNewTextChange() {
     setNewTaskInputText(event.target.value)
   }
 
-  function deleteTask (taskToDelete){
+  function deleteTask(taskToDelete) {
     const taskWithoutDeletedOne = task.filter(task => {
-      return task !== taskToDelete;
+      return task !== taskToDelete
     })
     setTask(taskWithoutDeletedOne)
   }
 
-  
-   return (
-    <div> 
+  return (
+    <div>
       <div className={styles.contetContainer}>
         <form onSubmit={handleCreateNewTask} className={styles.form}>
           <textarea
-              value={newTaskInputText}
-              name='taskText'
-              placeholder='Adicione uma nova tarefa'
-              onChange={handleNewTextChange}
+            value={newTaskInputText}
+            name="taskText"
+            placeholder="Adicione uma nova tarefa"
+            onChange={handleNewTextChange}
           />
-            <button className={styles.create}>Criar
-              <img className={styles.contentImgSizePlus} src='../plus.png' alt="" />
-            </button>
+          <button className={styles.create}>
+            Criar
+            <img
+              className={styles.contentImgSizePlus}
+              src="../plus.png"
+              alt=""
+            />
+          </button>
         </form>
-    </div>
-    <div className={styles.taskContent}>
-      <header className={styles.taskContentContainer}>
-        <div className={styles.headerContainer}>
-          <div className={styles.headerTaskContainer}>
-            <strong className={styles.createdCounterTaskContent}>tarefas criadas
-          
-           </strong>
-             <span className={styles.contentSpan} id='value'>0</span>
-          </div>
-         <div className={styles.headerTaskContainer}>
-            <strong className={styles.doneCounterTaskContent}>Concluidas
-            
-            </strong>
-            <span className={styles.contentSpan} id='value'>0</span>
-          </div>
-
-          
-        </div>
-          <div className={styles.taskContentContainerDiv}>
-            <div> 
-            {task.length !== 0 ?
-            task.map (task => {
-                 return <Task
-                    content={task}
-                    deleteTask={deleteTask}
-                 />
-              }) : <NoTask/> }
-            </div>
-  
-            
-          </div>
-      </header>
-      <div>
-
       </div>
-    </div>
+      <div className={styles.taskContent}>
+        <header className={styles.taskContentContainer}>
+          <div className={styles.headerContainer}>
+            <div className={styles.headerTaskContainer}>
+              <strong className={styles.createdCounterTaskContent}>
+                tarefas criadas
+              </strong>
+              <span className={styles.contentSpan} id="value">
+                {task.length}
+              </span>
+            </div>
+            <div className={styles.headerTaskContainer}>
+              <strong className={styles.doneCounterTaskContent}>
+                Concluidas
+              </strong>
+              <span className={styles.contentSpan} id="value">
+                0
+              </span>
+            </div>
+          </div>
+          <div className={styles.taskContentContainerDiv}>
+            <div>
+              {task.length !== 0 ? (
+                task.map(task => {
+                  return (
+                    <Task
+                      content={task}
+                      deleteTask={deleteTask}
+                      toggleToConcludeTask={checked}
+                    />
+                  )
+                })
+              ) : (
+                <NoTask />
+              )}
+            </div>
+          </div>
+        </header>
+        <div></div>
+      </div>
     </div>
   )
 }
